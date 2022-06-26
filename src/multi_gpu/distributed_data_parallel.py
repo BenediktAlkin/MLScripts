b5a99e99a0a32dbab4614726512e7c86df4d119b
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-06-26 18:09:32 +0200

    a

---
+++ src/multi_gpu/distributed_data_parallel.py
@@ -0,0 +1,19 @@
+import kappaprofiler as kp
+import torch
+import torch.nn as nn
+import yaml
+import util
+
+def main():
+    model = util.Model()
+    model = nn.parallel.DistributedDataParallel(model)
+    model.to(torch.device("cuda:0"))
+    optimizer = util.get_optim(model)
+    util.train(model, optimizer, only_single_batch=True)
+    with kp.Stopwatch() as sw:
+        util.train(model, optimizer)
+    print(sw.elapsed_seconds)
+
+
+if __name__ == "__main__":
+    main()
