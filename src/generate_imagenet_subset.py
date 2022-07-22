76d53f57c5aad1e27d8366c7203f0cb185a749a5
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-07-22 14:23:41 +0200

    Create generate_imagenet_subset.py

---
+++ src/generate_imagenet_subset.py
@@ -0,0 +1,25 @@
+from torchvision.datasets import ImageFolder
+import numpy as np
+from pathlib import Path
+import shutil
+
+def main():
+    main_impl("train")
+
+def main_impl(split):
+    keep_percentage = 0.01
+    root = Path(f"C:/Users/Benedikt Alkin/Documents/data/ImageNet/{split}")
+    ds = ImageFolder(root)
+    n_keep = int(len(ds) * keep_percentage)
+    rng = np.random.default_rng(seed=5)
+    idxs_to_keep = rng.choice(len(ds), n_keep, replace=False)
+    for idx in idxs_to_keep:
+        src_path, _ = ds.samples[idx]
+        src_path = Path(src_path).relative_to(root)
+        target_path = Path(f"{str(root.parent)}_{keep_percentage}") / src_path
+        target_path.parent.mkdir(exist_ok=True, parents=True)
+        shutil.copyfile(root / src_path, target_path)
+
+
+if __name__ == "__main__":
+    main()
