7144d5fd58ead8a2c29cdc311b8d7c4686b657af
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-07-25 20:21:09 +0200

    Update distributed_sampler.py

--- src/multi_gpu/distributed_sampler.py
+++ src/multi_gpu/distributed_sampler.py
@@ -24,7 +24,7 @@ def iterate(rank, loader):
 def main(rank):
     import os
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "55554"
+    os.environ["MASTER_PORT"] = "55553"
     dist.init_process_group(backend="nccl", init_method='env://', world_size=4, rank=rank)
 
     for size, batch_size in [
