7eb2f8a0af6eabc2e0a2b73d8d6a76acb72befd5
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-07-25 20:03:30 +0200

    Create distributed_sampler.py

---
+++ src/multi_gpu/distributed_sampler.py
@@ -0,0 +1,51 @@
+import torch
+from torch.utils.data import DataLoader, DistributedSampler, Dataset
+from torch.multiprocessing import spawn
+
+
+class DummyDataset(Dataset):
+    def __init__(self, size):
+        super().__init__()
+        self.size = size
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        return idx
+
+def iterate(rank, loader):
+    batches = torch.cat([batch for batch in loader])
+    print(f"{rank}: {len(batches)}")
+
+
+def main(rank):
+    import os
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "55554"
+    import torch.distributed as dist
+    dist.init_process_group(backend="nccl", init_method='env://', world_size=4, rank=rank)
+    ds = DummyDataset(size=1000)
+
+    dist.barrier()
+    if rank == 0: print("sampler=None drop_last=True")
+    iterate(DataLoader(ds, batch_size=128, sampler=None, drop_last=True))
+    dist.barrier()
+    if rank == 0: print("sampler=None drop_last=False")
+    iterate(DataLoader(ds, batch_size=128, sampler=None, drop_last=False))
+    dist.barrier()
+    if rank == 0: print("sampler=(drop_last=False) drop_last=False")
+    iterate(DataLoader(ds, batch_size=128, sampler=DistributedSampler(ds, drop_last=False), drop_last=False))
+    dist.barrier()
+    if rank == 0: print("sampler=(drop_last=True) drop_last=False")
+    iterate(DataLoader(ds, batch_size=128, sampler=DistributedSampler(ds, drop_last=True), drop_last=False))
+    dist.barrier()
+    if rank == 0: print("sampler=(drop_last=False) drop_last=True")
+    iterate(DataLoader(ds, batch_size=128, sampler=DistributedSampler(ds, drop_last=True), drop_last=True))
+    dist.barrier()
+    if rank == 0: print("sampler=(drop_last=True) drop_last=True")
+    iterate(DataLoader(ds, batch_size=128, sampler=DistributedSampler(ds, drop_last=True), drop_last=True))
+    dist.barrier()
+
+if __name__ == "__main__":
+    spawn(main, nprocs=4)
