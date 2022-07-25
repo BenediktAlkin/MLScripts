3cbddaeba23fbfcde9523ab3ccf3efa843b25d10
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-07-25 20:41:25 +0200

    Update distributed_sampler.py

--- src/multi_gpu/distributed_sampler.py
+++ src/multi_gpu/distributed_sampler.py
@@ -18,10 +18,13 @@ class DummyDataset(Dataset):
 def iterate(rank, loader):
     x = torch.cat([batch for batch in loader])
     print(f"{rank}: {[i.item() for i in x]}")
-    gathered = [torch.zeros_like(x) for _ in range(dist.get_world_size())]
-    dist.gather(x, gathered)
-    gathered = torch.concat(gathered)
-    print(f"{rank} all: {gathered}")
+    if rank == 0:
+        gathered = [torch.zeros_like(x) for _ in range(dist.get_world_size())]
+        dist.gather(x, gathered)
+        gathered = torch.concat(gathered)
+        print(f"{rank} all: {gathered}")
+    else:
+        dist.gather(x)
 
 
 def main(rank):
