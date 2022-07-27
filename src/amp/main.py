ff27cc892e4db2df60f4171e8c990827106fab0f
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2022-07-27 22:50:36 +0200

    Create main.py

---
+++ src/amp/main.py
@@ -0,0 +1,28 @@
+import torch
+
+
+def main():
+    torch.manual_seed(5)
+    model = torch.nn.Linear(10, 1)
+    optim = torch.optim.SGD(model.parameters(), lr=0.1)
+    loss_fn = torch.nn.MSELoss()
+    scaler = torch.cuda.amp.GradScaler()
+
+    x = torch.randn(5, 10)
+    with torch.cuda.amp.autocast():
+        y_hat = model(x)
+        loss = loss_fn(y_hat, torch.randn_like(y_hat))
+    print(f"loss: {loss}")
+    scaled_loss = scaler.scale(loss)
+    print(f"scaled_loss: {scaled_loss}")
+
+    print(model.weight)
+    optim.zero_grad()
+    scaled_loss.backward()
+    optim.step()
+    print(model.weight)
+    print(model.weight.grad)
+
+
+if __name__ == "__main__":
+    main()
